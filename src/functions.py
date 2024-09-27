11fa75a9250cf70eb61b183e789e5ca10c92048c
Joshua Oyedemi <josh.oyed@gmail.com>
2024-09-27 12:41:59 -0600

    functions file has been added

---
+++ src/functions.py
@@ -0,0 +1,19 @@
+
+def launch_angle_range(ve_v0, alpha, tol_alpha):
+ 
+
+
+ """Description of function.
+ Parameters
+ ----------
+ Returns
+ -------
+ """
+
+ 
+ # ...
+ # implementation of Equations (17) and (18)
+ # ...
+ #return phi_range : (phi_low, phi_upper)
+
+ 
