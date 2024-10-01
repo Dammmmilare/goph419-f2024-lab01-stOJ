c8635d437383336fa8efd700aae6d4b89d88b849
Joshua Oyedemi <josh.oyed@gmail.com>
2024-09-30 23:01:55 -0600

    code added to functions.py

--- src/functions.py
+++ src/functions.py
@@ -13,6 +13,35 @@ def launch_angle_range(ve_v0, alpha, tol_alpha):
  
  # ...
  # implementation of Equations (17) and (18)
+
+import numpy as np
+
+def asin(x):
+    
+    """
+    Compute inverse sine function
+    Input:  
+    x: float
+    Returns: float
+    """
+    
+    sign = -1 if x < 0 else 1
+    x = np.abs(x)
+    eps_a = 1.0
+    tol = 1e-8  # error tolerance
+    n = 1
+
+    result = 0.0
+    while eps_a > tol:
+        dy = (np.math.factorial(2*n) / (4**n * np.math.factorial(n)**2 * (2*n + 1))) * x**(2*n + 1)
+        result += dy
+        eps_a = np.abs(dy / result)
+        n += 1
+
+    result = sign * np.sqrt(0.5 * result)
+    return result
+
+
  # ...
  #return phi_range : (phi_low, phi_upper)
 
