1a3edeba54ff30e5e277d4dfadb473f16fa2f57d
Joshua Oyedemi <josh.oyed@gmail.com>
2024-09-30 23:31:55 -0600

    latest updates

--- src/functions.py
+++ src/functions.py
@@ -1,23 +1,45 @@
+import numpy as np
 
 def launch_angle_range(ve_v0, alpha, tol_alpha):
  
 
 
- """Description of function.
+ """
+ 
+ Description of function.
+ Computing the range of launch angles for the values of ve_vo, alpha, and tol_alpha.
+
  Parameters
  ----------
+ ve_vo : float
+    Ratio of escape velovity to the terminal velocity.
+
+ alpha : float
+    the desires maximum altitude with respect to the fraction of the earths radius.
+
+ tol_alpha : float
+    The tolerance for maximum altitude.
+
+
  Returns
  -------
+
+ numpy.array 
+
+    A two-component array containing the minimum and maximum desirable launch angles.
+
  """
 
- 
- # ...
- # implementation of Equations (17) and (18)
+
+
+ ### ...
+ ### implementation of Equations (17) and (18)
 
 import numpy as np
 
 def asin(x):
-    
+
+
     """
     Compute inverse sine function
     Input:  
@@ -25,6 +47,7 @@ def asin(x):
     Returns: float
     """
     
+
     sign = -1 if x < 0 else 1
     x = np.abs(x)
     eps_a = 1.0
@@ -33,8 +56,7 @@ def asin(x):
 
     result = 0.0
     while eps_a > tol:
-        dy = (((2*x)^2*n)/(n^2(np.math.factorial(2*n)/(np.math.factorial(n)^2))))                
-        #(np.math.factorial(2*n) / (4**n * np.math.factorial(n)**2 * (2*n + 1))) * x**(2*n + 1)
+        dy = (((2*x)**(2*n)) / (n**2 * (np.math.factorial(2*n) / (np.math.factorial(n)**2))))
         result += dy
         eps_a = np.abs(dy / result)
         n += 1
@@ -43,7 +65,10 @@ def asin(x):
     return result
 
 
+
  # ...
+
+
  #return phi_range : (phi_low, phi_upper)
 
  
