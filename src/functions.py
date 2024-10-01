d9921d8e916975c8f4b61fab033609e1f260b705
Joshua Oyedemi <josh.oyed@gmail.com>
2024-09-30 23:13:47 -0600

    code for the arcsin finished

--- src/functions.py
+++ src/functions.py
@@ -28,12 +28,13 @@ def asin(x):
     sign = -1 if x < 0 else 1
     x = np.abs(x)
     eps_a = 1.0
-    tol = 1e-8  # error tolerance
+    tol = 1e-8  # error tolerance, Es = 0.5 * 10 ^ -N
     n = 1
 
     result = 0.0
     while eps_a > tol:
-        dy = (np.math.factorial(2*n) / (4**n * np.math.factorial(n)**2 * (2*n + 1))) * x**(2*n + 1)
+        dy = (((2*x)^2*n)/(n^2(np.math.factorial(2*n)/(np.math.factorial(n)^2))))                
+        #(np.math.factorial(2*n) / (4**n * np.math.factorial(n)**2 * (2*n + 1))) * x**(2*n + 1)
         result += dy
         eps_a = np.abs(dy / result)
         n += 1
